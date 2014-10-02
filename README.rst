c7ae08b139b83b89c289ecc8b438f552954c31e2
Tryggvi Bjorgvinsson <tryggvi.bjorgvinsson@okfn.org>
2014-10-02 14:15:59 +0000

    Version 0.0.1
    Create setup file to make budgetdatapackage installable from pypi

--- README.rst
+++ README.rst
@@ -1,6 +1,12 @@
-# budgetdatapackage
+budgetdatapackage
+=================
 
-Extension of [datapackage](https://github.com/tryggvib/datapackage) which provides a ``BudgetResource`` class for creating a [Budget Data Package](https://github.com/openspending/budget-data-package) resource which is a drop in replacement for ``datapackage.Resource``.
+Extension of `datapackage <https://github.com/tryggvib/datapackage>`__
+which provides a ``BudgetResource`` class for creating a `Budget Data
+Package <https://github.com/openspending/budget-data-package>`__
+resource which is a drop in replacement for ``datapackage.Resource``.
+
+::
 
     from budgetdatapacakge import BudgetResource
     from datapackage import DataPackage
@@ -20,7 +26,8 @@ Extension of [datapackage](https://github.com/tryggvib/datapackage) which provid
     budget_dpkg = DataPackage(
         name="icelandic-budget", license="PDDL", resources=[budget])
 
-## License
+License
+-------
 
-budgetdatapackage is available under the GNU General Public License, version
-3. See LICENCE for more details.
+budgetdatapackage is available under the GNU General Public License,
+version 3. See LICENCE for more details.
---
+++ setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+description = "Manage and load Budget Data Packages"
+with open('README.rst') as readme:
+    long_description = readme.read()
+
+setup(
+    name = 'budgetdatapackage',
+    version = '0.0.1',
+    url = 'https://github.com/tryggvib/budgetdatapackage',
+    license = 'GPLv3',
+    description = description,
+    long_description = long_description,
+    maintainer = 'Tryggvi Björgvinsson',
+    maintainer_email = 'tryggvi.bjorgvinsson@okfn.org',
+    install_requires = ['datapackage>=0.4.1'],
+    packages = ['budgetdatapackage'],
+    package_dir={'budgetdatapackage': 'budgetdatapackage'},
+    package_data={'budgetdatapackage': ['data/*.json']},
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+    ],
+)
